ffbdd6080364e98c95c06c630eb19036bd49d5db
Swizec Teller <swizec@swizec.com>
2017-11-23 23:51:53 -0800

    Try to fix drip form

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,14 +3,16 @@ const FBPixel = require("react-facebook-pixel");
 exports.onClientEntry = function() {
     var _dcq = _dcq || [];
     var _dcs = _dcs || {};
-    _dcs.account = "8481241";
+    _dcs.account = "4947882";
 
-    var dc = document.createElement("script");
-    dc.type = "text/javascript";
-    dc.async = true;
-    dc.src = "//tag.getdrip.com/8481241.js";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(dc, s);
+    (function() {
+        var dc = document.createElement("script");
+        dc.type = "text/javascript";
+        dc.async = true;
+        dc.src = "//tag.getdrip.com/4947882.js";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(dc, s);
+    })();
 
     FBPixel.init(714190382013726);
     FBPixel.pageView();
