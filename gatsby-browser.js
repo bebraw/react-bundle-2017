db7f6cef6184674ffabeda0beb20530142deba47
Swizec Teller <swizec@swizec.com>
2017-11-23 16:58:50 -0800

    Add FB pixel

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,17 @@
-exports.onClientEntry = function() {
-    console.log("HAI THERE");
+const FBPixel = require("react-facebook-pixel");
 
+exports.onClientEntry = function() {
     var _dcq = _dcq || [];
     var _dcs = _dcs || {};
     _dcs.account = "8481241";
 
-    (function() {
-        var dc = document.createElement("script");
-        dc.type = "text/javascript";
-        dc.async = true;
-        dc.src = "//tag.getdrip.com/8481241.js";
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(dc, s);
-    })();
+    var dc = document.createElement("script");
+    dc.type = "text/javascript";
+    dc.async = true;
+    dc.src = "//tag.getdrip.com/8481241.js";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(dc, s);
+
+    FBPixel.init(714190382013726);
+    FBPixel.pageView();
 };
